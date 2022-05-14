35ecefcdb6303f3d70af4218bbd66309739a1aa1
Samantha Grey <49354894+sammygrey@users.noreply.github.com>
2022-05-14 01:25:26 -0400

    recursion problems finished

--- recursion/problem1.py
+++ recursion/problem1.py
@@ -3,3 +3,33 @@
 # If the target string is not found in the array, report that.
 # Example input:  instructors = [Adriana, Adriana, Alan, Alan, Alan, Alan, Alan, Braus, Braus, Braus, Braus, Dan, Dan, Dan, Dan, Dan, Dani, Dani, Jess, Meredith, Milad, Milad, Mitchell, Mitchell, Mitchell, Mitchell]
 # Example execution:  find_indexes(instructors, 'Braus')  ⇒  (7, 10)
+
+first = None
+last = None
+
+def recursive_find(arr, item, i=0):
+    global first, last
+
+    if i >= len(arr):
+        return
+
+    if arr[i] == item:
+
+        if first == None:
+            first = i
+
+        elif i+1 < len(arr) and arr[i+1]!=item:
+            last = i
+
+        elif i+1 >= len(arr):
+            last = i
+
+    i += 1
+    recursive_find(arr, item, i)
+
+instructors = ['Adriana', 'Adriana', 'Alan', 'Alan', 'Alan', 'Alan', 'Alan', 'Braus', 'Braus', 'Braus', 'Braus', 'Dan', 'Dan',
+               'Dan', 'Dan', 'Dan', 'Dani', 'Dani', 'Jess', 'Meredith', 'Milad', 'Milad', 'Mitchell', 'Mitchell', 'Mitchell', 'Mitchell']
+
+recursive_find(instructors, 'Braus')
+
+print(first,last)
--- recursion/problem2.py
+++ recursion/problem2.py
@@ -2,3 +2,27 @@
 # return a list of all letter combinations they could have been trying to type on the keypad.
 # Example execution 1:  t9_letters("23")  ⇒  ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
 # Example execution 2:  t9_letters("4663")  ⇒  ["gmmd", …, "gone", …, "good", …, "home", …, "hood", …, "ioof"]
+
+nums = "4663"
+results = []
+digits = {"2": "abc",
+          "3": "def",
+          "4": "ghi",
+          "5": "jkl",
+          "6": "mno",
+          "7": "qprs",
+          "8": "tuv",
+          "9": "wxyz", }
+
+
+def t9_letters(i=0, currentString=""):
+    if len(currentString) == len(nums):
+        results.append(currentString)
+        return
+
+    for char in digits[nums[i]]:
+        t9_letters(i+1, currentString + char)
+
+
+t9_letters()
+print(results)
